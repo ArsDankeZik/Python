e1f9f23109460ecfbdbaa1027a83edef6f4ad782
DankeZik <dankezik@openmailbox.org>
2017-11-07 21:05:55 +0100

    Auto stash before merge of "master" and "ArsLonga/master"

---
+++ Python/ededs_1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+2. Pedir un número e indicar si es positivo o negativo.
+"""
+nr = int(input("Introduzca un número entero: "))
+
+if nr >= 0:
+    print("El número es positivo")
+else:
+    print("El número es negativo")
+print("Espero que vuelva pronto")
