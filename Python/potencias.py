a6d16c87b41e5c280063ced04e0641e02a2e826b
DankeZik <dankezik@openmailbox.org>
2017-11-13 22:18:55 +0100

    Add files via upload

---
+++ Python/potencias.py
@@ -0,0 +1,20 @@
+print("Este programa calculará la potencia de una base.\n¿Desea continuar con la ejecución del programa?")
+respuesta = input("s/n: ")
+
+while respuesta.lower() in ("s"):
+	base = int(input("Introduzca la base: "))
+	exponente = int(input("Introduzca el exponente: ")) 
+
+	i = 1
+	if exponente == 0:
+		print("El resultado es 1.")
+		respuesta = input("¿Quiere repetir?(s/n): ")
+	else:
+
+		while i < exponente:
+			resultado = base**exponente
+			print ("El resulatado es",resultado)
+			respuesta = input("¿Quiere repetir?(s/n): ")
+			break
+else:
+	print("Nos vemos.")
